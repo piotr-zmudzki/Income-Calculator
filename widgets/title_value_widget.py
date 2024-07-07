397e6277d55d2c7a7be0b8f1c0b71ff234d670d4
Piotrek <ktablet659@gmail.com>
2024-07-07 18:28:37 +0200

    Create title_value_widget.py

---
+++ widgets/title_value_widget.py
@@ -0,0 +1,5 @@
+#this will be a widget that provides a name (const) and a value (variable)
+#something like this:
+# Income generated in this month (name): [red] -145.43zł (variable)
+# additional argument: variable_color
+# additional func: variable_update - which will re-set the variable part so it get's refreshed
