f56c8c77213a53b986cf4aa8aa0513796fc3321e
Piotrek <ktablet659@gmail.com>
2024-07-13 15:13:54 +0200

    Make main_font more readable

--- main_font.py
+++ main_font.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-#prefered_font = ctk.CTkFont(family="Product Sans", size=30)
+
 def get_prefered_font():
-    product_sans = ctk.CTkFont(family="Product Sans", size=30)
-    return product_sans
+    font = ctk.CTkFont(family="Product Sans", size=30)
+    return font
